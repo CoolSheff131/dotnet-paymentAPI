c244cf91923c09991d6dac2f6507a9c80b048bdd
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-04-17 12:34:58 +0800

    dbContext

--- dotnet-paymentAPI/Startup.cs
+++ dotnet-paymentAPI/Startup.cs
@@ -1,5 +1,7 @@
+using dotnet_paymentAPI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,11 @@ namespace dotnet_paymentAPI
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddRazorPages();
+
+			services.AddDbContext<PaymentDetailContext>(options =>
+			{
+				options.UseSqlServer(Configuration.GetConnectionString("DevConnection"));
+			});
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
