e77a17c8ce2605a577f8e97880bbe7c4d14dbcd1
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-04-17 13:25:09 +0800

    Controller

--- dotnet-paymentAPI/Startup.cs
+++ dotnet-paymentAPI/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,12 +25,16 @@ namespace dotnet_paymentAPI
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddRazorPages();
-
+			services.AddControllers();
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new OpenApiInfo { Title = "PaymentAPI", Version = "v1" });
+			});
 			services.AddDbContext<PaymentDetailContext>(options =>
 			{
 				options.UseSqlServer(Configuration.GetConnectionString("DevConnection"));
 			});
+
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,7 +57,7 @@ namespace dotnet_paymentAPI
 
 			app.UseEndpoints(endpoints =>
 			{
-				endpoints.MapRazorPages();
+				endpoints.MapControllers();
 			});
 		}
 	}
